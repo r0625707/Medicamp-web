822e15bceaba4b30f69780c3c1ff2bac7dcdeb1a
r0625707 <bram.vanasschodt@student.ucll.be>
2018-03-06 20:16:30 +0100

    firebase app

--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -145,29 +145,6 @@ class UserForm extends React.Component {
                                 <Label for="passwordrep">Herhaal wachtwoord*</Label>
                                 <Input type="password" name="passwordrep" id="passwordrep" value={this.state.passwordrep} onChange={this.onPasswordrepChange} />
                             </FormGroup>
-                            <FormGroup tag="fieldset">
-                                <legend>Adres*</legend>
-                                <FormGroup>
-                                    <Label for="straat">Straatnaam</Label>
-                                    <Input type="text" name="straat" id="straat" value={this.state.straat} onChange={this.onStraatChange} />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="huisnr">Huisnr.</Label>
-                                    <Input type="number" name="huisnr" id="huisnr" value={this.state.huisnr} onChange={this.onHuisnrChange} />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="bus">Bus</Label>
-                                    <Input type="text" name="bus" id="bus" value={this.state.bus} onChange={this.onBusChange} />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="postcode">Postcode</Label>
-                                    <Input type="text" name="postcode" id="postcode" value={this.state.postcode} onChange={this.onPostcodeChange} />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="plaats">Plaats</Label>
-                                    <Input type="text" name="plaats" id="plaats" value={this.state.plaats} onChange={this.onPlaatsChange} />
-                                </FormGroup>
-                            </FormGroup>
                         </Form>
                         <p>* velden zijn verplicht </p>
                     </ModalBody>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyAC-LVJeWkMmtdJ-8XxVqOKKSiVqRMEOX0",
+    authDomain: "medicamp-so.firebaseapp.com",
+    databaseURL: "https://medicamp-so.firebaseio.com",
+    projectId: "medicamp-so",
+    storageBucket: "medicamp-so.appspot.com",
+    messagingSenderId: "787943796754"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(
     <BrowserRouter>
